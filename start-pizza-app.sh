6223cea9f5fbdef13e4f168f3ec79ba099a704da
Pete Rodriguez <pedrorod@microsoft.com>
2023-03-14 13:12:03 +0000

    Updated start-pizza.sh

--- start-pizza-app.sh
+++ start-pizza-app.sh
@@ -6,12 +6,10 @@ export APP_BRAINTREE_OPTS="-Dbraintree.merchantId=$APP_BRAINTREE_MERCHANT_ID -Db
 
 export APP_RECAPTCHA_OPTS="-Drecaptcha.public.key=$APP_RECAPTCHA_PUBLIC_KEY -Drecaptcha.private.key=$APP_RECAPTCHA_PRIVATE_KEY"
 
-export APP_DATASOURCE_OPTS="-Ddatasource.driverClassName=$APP_DATASOURCE_DRIVER -Ddatasource.url=$APP_DATASOURCE_URL -Ddatasource.username=$APP_DATASOURCE_USERNAME -Ddatasource.password=$APP_DATASOURCE_PASSWORD"
-
-export APP_HIBERNATE_OPTS="-Dhibernate.dialect=$APP_HIBERNATE_DIALECT -Dhibernate.hbm2ddl.auto=$APP_HIBERNATE_HBM2DDL_AUTO -Dhibernate.default_schema=$APP_HIBERNATE_DEFAULT_SCHEMA"
-
 # This loads up all the environment variables as Java System properties
-export PIZZA_JAVA_OPTIONS="$APP_BRAINTREE_OPTS $APP_RECAPTCHA_OPTS $APP_DATASOURCE_OPTS $APP_HIBERNATE_OPTS"
+export PIZZA_JAVA_OPTIONS="$APP_BRAINTREE_OPTS $APP_RECAPTCHA_OPTS"
+export APP_PORT=8081
+export APP_CONTEXT_PATH=pizzeria
 
 # Show the environment variables at runtime
 echo "Display all environment variables at runtime"
